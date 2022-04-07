60b4e933ab1ada6f2c43c9e026844d2b7b0d41c7
Saint <p.leelayuthto@hotmail.com>
2022-04-07 14:27:53 +0700

    OpenAPI with Swagger

--- server.js
+++ server.js
@@ -9,18 +9,43 @@ const rateLimit=require('express-rate-limit');
 const hpp=require('hpp');
 const cors=require('cors');
 
+const swaggerJsDoc =require('swagger-jsdoc');
+const swaggerUI =require('swagger-ui-express');
+
+const connectDB = require('./config/db');
+
 // Route files
 const hospitals = require('./routes/hospitals');
 const auth = require('./routes/auth');
 const appointments = require('./routes/appointments');
 
-const connectDB = require('./config/db');
 //Load env vars
 dotenv.config({path:'./config/config.env'});
 
 //Connect to database
 connectDB();
+
 const app=express();
+
+const swaggerOptions={
+    swaggerDefinition:{
+        openapi: '3.0.0',
+        info: {
+            title: 'Library API',
+            version: '1.0.0',
+            description: 'A simple Express VacQ API'
+        },
+        servers: [
+            {
+                url: 'http://localhost:5050/api/v1'
+            }
+        ]
+    },
+    apis:['./routes/*.js'],
+};
+
+const swaggerDocs=swaggerJsDoc(swaggerOptions);
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocs));
 
 //Body parser
 app.use(express.json());
@@ -55,7 +80,7 @@ app.use('/api/v1/hospitals',hospitals);
 app.use('/api/v1/appointments',appointments);
 app.use('/api/v1/auth',auth);
 
-const PORT=process.env.PORT || 3000;
+const PORT=process.env.PORT || 5050;
 const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
 
 //Handle unhandled promise rejections
